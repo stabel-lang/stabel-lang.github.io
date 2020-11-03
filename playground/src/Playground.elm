af09bfa1f478185a0a18940e338b55c1e579b9a5
robin.hansen <>
2020-11-03 04:47:09 +0000

    Setup elm architecture in playground.
    FossilOrigin-Name: 50d720c73fe9a546228fb82a13d11400ddd64f7d487298be433ea4f5fa724129

--- playground/src/Playground.elm
+++ playground/src/Playground.elm
@@ -1,8 +1,55 @@
 module Playground exposing (main)
 
+import Browser
 import Html exposing (Html)
 
 
-main : Html a
+
+-- MODEL
+
+
+type alias Model =
+    { source : String }
+
+
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( { source = "" }
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    Html.text "Hello"
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
 main =
-    Html.text "Hello World!"
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
