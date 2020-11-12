9871dcecdee5480d35666352e5201db6d378d56d
robin.hansen <>
2020-11-12 05:08:10 +0000

    Add build step to frontpage piece of part
    FossilOrigin-Name: 95afcaaade2573250fcb6ed04e31f91a1a25647edbd399c68ca0342243cab20e

--- deploy.sh
+++ deploy.sh
@@ -13,17 +13,21 @@ npm run build
 popd
 
 # Build frontpage
-echo "copying frontpage/* into dist"
-cp frontpage/* dist
+pushd frontpage
+
+npm ci
+npm run build
+
+popd
 
 # Upload to s3
 pushd dist
 
 aws s3 cp ./index.html s3://play-website \
-    --cache-control must-revalidate \
+    --cache-control must-revalidate
 
 aws s3 cp ./playground.html s3://play-website \
-    --cache-control must-revalidate \
+    --cache-control must-revalidate
 
 aws s3 cp ./playground.*.js s3://play-website \
     --cache-control immutable
