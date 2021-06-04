04e112a0eaaf1e502f08e2bd374809409f105cd1
robin.hansen <>
2021-06-04 11:11:52 +0000

    Rebrand from Play to Stabel
    FossilOrigin-Name: c896d3739b535f1caf9693afc24a7bbb0e7e62a6c916462844eea9c6f08affb2

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 # Upload to s3
 pushd dist
 
-aws s3 cp ./index.html s3://play-website \
+aws s3 cp ./index.html s3://stabel-website \
     --cache-control max-age=600
 
-aws s3 cp ./*.css s3://play-website \
+aws s3 cp ./*.css s3://stabel-website \
     --cache-control immutable
 
-aws s3 cp ./playground.html s3://play-website \
+aws s3 cp ./playground.html s3://stabel-website \
     --cache-control max-age=600
 
-aws s3 cp ./*.js s3://play-website \
+aws s3 cp ./*.js s3://stabel-website \
     --cache-control immutable
 
 popd
