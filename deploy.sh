4ed8c2f1ef077a81d83535517739f3aa25fe87e0
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-04-12 08:48:38 +0200

    Make project work as github page.

--- deploy.sh
+++ deploy.sh
@@ -7,28 +7,6 @@ rm -r .cache || true
 npm ci
 npm run build
 
-# Upload to s3
-pushd dist
-
-aws s3 cp ./index.html s3://stabel-website \
-    --cache-control max-age=3600
-
-aws s3 cp ./community.html s3://stabel-website \
-    --cache-control max-age=3600
-
-aws s3 cp ./playground.html s3://stabel-website \
-    --cache-control max-age=3600
-
-aws s3 cp ./*.css s3://stabel-website \
-    --cache-control max-age=3600
-
-aws s3 cp ./*.js s3://stabel-website \
-    --cache-control immutable
-
-aws s3 cp ./*.ico s3://stabel-website \
-    --cache-control immutable
-
-aws s3 cp ./*.png s3://stabel-website \
-    --cache-control immutable
-
-popd
+rm -r docs || true
+mkdir -p docs
+cp -R dist/* docs
